e0bda9c29ccffd6d237e046c9c6a32986b4be069
Supun Perera <dmxunlimit@gmail.com>
2021-03-02 09:03:48 +0530

    script updates

--- loadTestSuite/loadTestSuiteRunner.sh
+++ loadTestSuite/loadTestSuiteRunner.sh
@@ -3,7 +3,7 @@
 CURRENTDIR=$(pwd)
 mkdir -p $CURRENTDIR/tools/
 
-echo "Loading the latest script updates .. \n"
+echo "Loading the script updates .. \n"
 curl -s https://raw.githubusercontent.com/dmxunlimit/tools/master/loadTestSuite/scripts/stop.sh -o stop.sh
 sudo chmod 755 stop.sh
 
