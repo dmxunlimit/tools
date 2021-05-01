31c2653325379186205fba0be7c3d4e37a1bed9a
Supun Perera <dmxunlimit@gmail.com>
2021-05-01 19:46:39 +0530

    support windows

--- git-tools/git-clone-product-is.sh
+++ git-tools/git-clone-product-is.sh
@@ -3,6 +3,17 @@
 # usage : ./git-checkout <product_version> <force>
 # usage : ./git-checkout 5.2.0 y
 
+# Detect the platform (similar to $OSTYPE)
+OS="$(uname)"
+case $OS in
+'Linux')
+    git config --global credential.helper cache
+    ;;
+*NT*)
+    git config --system core.longpaths true
+    ;;
+esac
+
 ##### Automated script update #####
 
 script_dir=$(dirname "$0")
@@ -11,14 +22,11 @@ scriptFile="$script_dir/$scriptBaseName"
 scriptFilelst=$scriptFile"_latest"
 echo "Checking for latest version of the script $scriptBaseName !"
 
-curl -s https://raw.githubusercontent.com/dmxunlimit/tools/master/git-tools/$scriptBaseName -o $scriptFilelst
+curl -sf https://raw.githubusercontent.com/dmxunlimit/tools/master/git-tools/$scriptBaseName -o $scriptFilelst
 
 if [ -f "$scriptFilelst" ] && [ -s "$scriptFilelst" ]; then
-    # Detect the platform (similar to $OSTYPE)
-    OS="$(uname)"
     case $OS in
     'Linux')
-        OS='Linux'
         alias ls='ls --color=auto'
         crr_md5=$(md5sum $scriptFile)
         remt_md5=$(md5sum $scriptFilelst)
@@ -26,11 +34,16 @@ if [ -f "$scriptFilelst" ] && [ -s "$scriptFilelst" ]; then
         remt_md5=$(echo $remt_md5 | cut -d " " -f1)
         ;;
     'Darwin')
-        OS='Mac'
         crr_md5=$(md5 $scriptFile)
         remt_md5=$(md5 $scriptFilelst)
         crr_md5=$(echo $crr_md5 | cut -d "=" -f2)
         remt_md5=$(echo $remt_md5 | cut -d "=" -f2)
+        ;;
+    *NT*)
+        crr_md5=$(md5sum $scriptFile)
+        remt_md5=$(md5sum $scriptFilelst)
+        crr_md5=$(echo $crr_md5 | cut -d " " -f1)
+        remt_md5=$(echo $remt_md5 | cut -d " " -f1)
         ;;
     *)
         crr_md5=$(md5sum $scriptFile)
@@ -54,8 +67,6 @@ fi
 ####
 
 CUR_DIR=$(pwd)
-
-git config --global credential.helper cache
 
 wrk_dir=$(pwd)
 mkdir -p $wrk_dir'/artefacts'
@@ -155,16 +166,16 @@ while read line || [ -n "$line" ]; do
             if [[ "$value" == *"jaggery.extensions.version"* ]]; then
                 value=$jag_version
             fi
-            
+
             cloneRepo
 
             final_key=$gitrepo"-"$prodVersion"='$value"
             echo $final_key >>$REPO_VERSIONS_FILE
 
-            
         fi
 
-    done <./temp-versions
+    done \
+        <./temp-versions
     echo "" >>$REPO_VERSIONS_FILE
 done <$PRODCT_VER_FILE
 
