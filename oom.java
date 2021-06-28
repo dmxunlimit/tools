d5084b267ff7971cb2573de53890169621d6fca0
Supun Perera <dmxunlimit@gmail.com>
2021-06-28 22:00:07 +0530

    Create oom.java

---
+++ oom.java
@@ -0,0 +1,22 @@
+public class OutOfMemoryErrorDemo {
+   public static void main(String[] args) throws Exception {
+      int dummyArraySize = 15;
+      System.out.println("Max JVM memory: " + Runtime.getRuntime().maxMemory());
+      long memoryConsumed = 0;
+      try {
+         long[] memoryAllocated = null;
+         for(int loop = 0; loop < Integer.MAX_VALUE; loop++) {
+            memoryAllocated = new long[dummyArraySize];
+            memoryAllocated[0] = 0;
+            memoryConsumed += dummyArraySize * Long.SIZE;
+            System.out.println("Memory Consumed till now: " + memoryConsumed);
+            dummyArraySize *= dummyArraySize * 2;
+            Thread.sleep(500);
+         }
+      } catch (OutOfMemoryError outofMemory) {
+         System.out.println("Catching out of memory error");
+         //Log the information, so that we can generate the statistics
+         throw outofMemory;
+      }
+   }
+}
