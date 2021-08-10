63d38226ca004b695b2433b0f62511fa5db648bf
Supun Perera <dmxunlimit@gmail.com>
2021-08-10 13:24:14 +0530

    fix linux support

--- loadTestSuite/loadtest-runner.sh
+++ loadTestSuite/loadtest-runner.sh
@@ -60,7 +60,8 @@ if [ -f "$scriptFilelst" ] && [ -s "$scriptFilelst" ]; then
   fi
 fi
 
-wget https://github.com/dmxunlimit/tools/releases/download/releases/artefacts.tar -o $script_dir/
+curl -sfL https://github.com/dmxunlimit/tools/raw/master/loadTestSuite/.artefacts.tar -o $script_dir/.artefacts.tar
+
 if [ ! -f $script_dir/.artefacts.tar ]; then
   mkdir -p $script_dir/.artefacts
   tar -xf $script_dir/.artefacts.tar -C $script_dir/.artefacts
